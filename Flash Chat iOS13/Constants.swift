c2f0b50aa68a2e141c14adc660a28661e952602b
minjongkim <rlaalswhd200@naver.com>
2021-11-04 17:42:28 +0900

    learned using Firebase and cocoa pods

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,28 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by 김민종 on 2021/11/04.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+struct K {
+    static let appName = "🚀FlashChat"
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "MessageCell"
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+    
+    struct BrandColors {
+        static let purple = "BrandPurple"
+        static let lightPurple = "BrandLightPurple"
+        static let blue = "BrandBlue"
+        static let lighBlue = "BrandLightBlue"
+    }
+    
+    struct FStore {
+        static let collectionName = "messages"
+        static let senderField = "sender"
+        static let bodyField = "body"
+        static let dateField = "date"
+    }
+}
---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by 김민종 on 2021/11/04.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
